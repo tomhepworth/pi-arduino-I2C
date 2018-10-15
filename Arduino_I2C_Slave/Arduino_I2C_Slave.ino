7f56c60e8837e0dae54b4415fbcd725ede2b8697
Thomas Hepworth <tomhepworth@hotmail.co.uk>
2018-10-15 11:33:46 +0100

    Refactored code, now works for analog read
    Serial prints in the sendData() function were causing problems with the interrupt service routine that meant the arduino would send back the wrong value. Note added about only using serial prints in specific areas. Code in general is now commented thanks to github.com/descartes, who also refactored a lot of code.

---
+++ Arduino_I2C_Slave/Arduino_I2C_Slave.ino
@@ -0,0 +1,80 @@
+#include <Wire.h>
+
+
+// I2C support
+#define SLAVE_ADDRESS 0x20
+int value = 0;
+byte readCount = 0;
+
+void setup() {
+  Serial.begin(57600);
+  Serial.print("Setup");
+
+
+  // Setup pins & initialise I2C as slave
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for I2C communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready");
+}
+
+void loop() {
+  //Serial.print("v: "); //use serial prints here to prevent conflicts with i2c and the interrupt service routine (see sendData())
+  //Serial.println(value); //note that value will only update when an i2c request is made
+  delay(100);
+}
+
+
+// callback for received data
+void receiveData(int byteCount){
+  int io;
+  // no "int value" as it is a global so analog values that are read can be transmitted
+
+  if (byteCount == 2) { // Write to a pin
+    io = Wire.read();
+    value = Wire.read();
+    pinMode(io, OUTPUT);
+
+    if (io > 128) { // If the io pin value has the bit 8 set, then it's an analogWrite request
+      analogWrite((io - 128),value);
+
+    } else {
+      digitalWrite(io,value);
+
+    }
+
+  } else if (byteCount == 1) {  // Read from a pin
+    io = Wire.read();
+    pinMode(io, INPUT);
+
+    if (io > 128) { // If the io pin value has the bit 8 set, then it's an analogRead request - you can digitalRead an analog pin except for A6 & A7 (20 & 21) which are analog only
+      value = analogRead(io - 128);
+      readCount = 0; // Override any current read count as the value has now changed - if the master hasn't read the two values, tough.
+
+    } else {
+      value = digitalRead(io);
+      readCount = 1; // This will jump straight to returning the LSB as we are only going to be returning a 0 or a 1 - rather up to the other end to ask for just one byte.
+    }
+  }
+}
+
+
+// callback for sending data
+void sendData(){
+  //IMPORTANT:  Avoid using serial prints in this area as they interfere with the interrupt service routine, slow down the arduino, and can corrupt the value being sent. Use them in the loop() function if required. 
+  
+  // As I2C only does 7 bit data, we have to chop up a value, return two parts and reassemble at the other end. The 'other end' has to perform two reads as the 'other end' is a Pi running Python and SMBus library has no simple way of receiving a word from a slave without using a register, which isn't supported in the Arduino Wire library.
+  int t = 0;
+  if (readCount == 0) {
+    t = value >> 8; // MSB
+    readCount = 1; // Set flag to send second half on next call
+  } else {
+    t = value & 255; // LSB
+    readCount = 0;
+  }
+}
---
+++ Pi_I2C_Master.py
@@ -0,0 +1,57 @@
+﻿from smbus2 import SMBus
+import time
+# for RPI version 1, use bus = smbus.SMBus(0)
+bus = SMBus(1)
+
+# This is the address we setup in the Arduino Program
+address = 0x20
+
+##bus.write_byte_data(address,io,value)
+def digitalRead(io):
+	bus.write_byte(address,io) 		## tells arduino code that we're reading
+	time.sleep(0.05) 				## wait to give time for the arduino to read
+	return bus.read_byte(address) 	## now get the value & return
+
+def analogRead(io):
+	bus.write_byte(address,io + 128) 	## set bit 8 high on io to do an analog read
+	time.sleep(0.1) 					## wait for time for the ADC to run
+	msb = bus.read_byte(address)		## get first half of value
+	lsb = bus.read_byte(address)		## get bottom half of value
+	return (msb * (2**8)) + lsb			## reassemble the value & return
+
+
+while True:
+	io = int(input("Enter io (2 - 21): "))
+	if not io:
+		continue
+
+	mode = input("Read or Write? r/w: ")
+
+	if(mode == "r"):
+		AorD = input("Analog or Digital? a/d: ")
+		if(AorD == "a"):
+			print(analogRead(io))
+
+		elif(AorD == "d"):
+			print(digitalRead(io))
+
+		else:
+			print("ERROR: invalid read type")
+
+	elif(mode == "w"):
+		ADorS = input("Analog, Digital? a/d:")
+		v = int(input("Enter a value: "))
+		if not v:
+			continue
+
+		if(AorD == "a"):
+			bus.write_byte_data(address,(io+128),v)	## set bit 8 to signal an analogWrite
+
+		elif(ADorS == "d"):
+			bus.write_byte_data(address,io,v)		## plain old digitalWrite
+
+		else:
+			print("ERROR: invalid write type")
+
+	else:
+		print("ERROR: invalid mode selected")
